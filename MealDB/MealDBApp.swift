5ef0c97d20cfd08a260945d7ef8e492b5fa9d351
Htain Lin Shwe <saturngod@gmail.com>
2024-04-19 14:03:15 -0500

    project setup for meal db

---
+++ MealDB/MealDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealDBApp.swift
+//  MealDB
+//
+//  Created by Htain Lin Shwe on 19/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MealDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
